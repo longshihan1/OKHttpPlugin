75aa50f1c22e57a0193744d0a8cdfdbf1b1c2183
longhe <longhe@imdada.cn>
2020-08-20 10:59:39 +0800

    删除无用配置

---
+++ app/src/main/java/com/longshihan/okhttpplugin/MainActivity.java
@@ -0,0 +1,14 @@
+package com.longshihan.okhttpplugin;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+    }
+}
